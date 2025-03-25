f35065938760e27920388e754f7a97c7dca6e413
Renspeters <1195839@student.roc-nijmegen.nl>
2025-03-25 09:06:43 +0100

    Crypto API toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,19 @@ import Market from './Market';
 
 export default function App(){
   return (
+    <>
+    <header>
+      <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/market">Market</a></li>
+        </ul>
+      </nav>
+    </header>
     <Routes>
       <Route path="/market" element={<Market />} />
     </Routes>
+    </>
   );
 }
 
--- src/Market.jsx
+++ src/Market.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
- 
+import Container from 'react-bootstrap/Container';
+import { Row } from 'react-bootstrap';
+
 const PriceCards = () => {
   const [coins, setCoins] = useState([]);
- 
+
   useEffect(() => {
     const fetchCoins = async () => {
       try {
@@ -12,7 +14,7 @@ const PriceCards = () => {
           'https://api.coingecko.com/api/v3/coins/markets',
           {
             params: {
-              vs_currency: 'z',
+              vs_currency: 'usd',
               order: 'market_cap_desc',
               per_page: 50,
               page: 1,
@@ -25,27 +27,38 @@ const PriceCards = () => {
         console.error('Error fetching coin data:', error);
       }
     };
- 
+
     fetchCoins();
   }, []);
- 
+
   return (
-    <div>
-      {coins.map((coin) => (
-        <div key={coin.id} className="price-card">
-          <img src={coin.image} alt={coin.name} className="coin-image" />
-          <h3>{coin.name}</h3>
-          <p className="symbol">{coin.symbol.toUpperCase()}</p>
-          <div className={coin.price_change_percentage_24h > 0 ? 'positive' : 'negative'}>
-            <p className="price">${coin.current_price.toLocaleString()}</p>
-            <p className="price-change">
-              {coin.price_change_percentage_24h.toFixed(2)}%
-            </p>
+    <Container fluid>
+      <Row>
+      <h2 className="market-title">CryptoPulse current market</h2>
+      <div className="price-cards-container">
+        {coins.map((coin) => (
+          <div key={coin.id} className="price-card">
+            <img src={coin.image} alt={coin.name} className="coin-image" />
+            <h3 className="coin-name">{coin.name}</h3>
+            <p className="coin-symbol">{coin.symbol.toUpperCase()}</p>
+            <div
+              className={
+                coin.price_change_percentage_24h > 0 ? 'positive' : 'negative'
+              }
+            >
+              <p className="coin-price">
+                ${coin.current_price.toLocaleString()}
+              </p>
+              <p className="price-change">
+                {coin.price_change_percentage_24h.toFixed(2)}%
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+      </Row>
+    </Container>
   );
 };
- 
+
 export default PriceCards;
